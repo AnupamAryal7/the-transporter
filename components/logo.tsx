9047e2342be995fff61f233a865b70123a129ccb
AnupamAryal7 <anmaryal41@gmail.com>
2025-11-13 23:40:38 +0545

    the color of the theme changed to blue

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,12 +1,12 @@
-import { Package } from "lucide-react"
-import Link from "next/link"
-import { SITE_NAME } from "@/lib/constants"
+import { Package } from "lucide-react";
+import Link from "next/link";
+import { SITE_NAME } from "@/lib/constants";
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-bold">
-      <Package className="h-5 w-5 text-primary" />
-      <span className="text-primary">{SITE_NAME}</span>
+      <Package className="h-5 w-5 text-primary text-blue-950" />
+      <span className="text-primary text-blue-950">{SITE_NAME}</span>
     </Link>
-  )
+  );
 }
